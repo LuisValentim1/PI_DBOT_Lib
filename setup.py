d78a2c6442e60671ab232cf88081d05fd1ef9021
unknown <lvalentim@ua.pt>
2021-04-27 15:26:39 +0100

    Adicionar setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+  
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+      name='Libs',
+      version='0.1',
+      description='DBOT Libs',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Luís Valentim',
+      author_email='lvalentim@ua.pt',
+      url='https://github.com/LuisValentim1/PI_DBOT_Lib',
+      packages=find_packages(),
+      install_requires=['Cassandra>=3.11.10']
+)
