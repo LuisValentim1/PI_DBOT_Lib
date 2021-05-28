724643b8976842c0c89200abdb64e628391acf80
unknown <lvalentim@ua.pt>
2021-05-28 15:21:41 +0100

    testes de Cache / pequenos ajustes

--- DBoT/Cache.py
+++ DBoT/Cache.py
@@ -6,26 +6,26 @@ class Cache:
 
     def __init__(self, cacheSize):
         self.cachedElements = dict()
-        self.timeout = 100000
+        self.timeout = 3000
         self.size = cacheSize
     
     def add(self, user, session):
-        if user not in cachedElements:
-            if len(cachedElements) < self.size:
+        if user not in self.cachedElements:
+            if len(self.cachedElements) < self.size:
                 self.cachedElements[user] = [session, time.time()]
             else:
-                self.cachedElements.pop(self.cachedElements.keys()[0])
+                self.cachedElements.pop(list(self.cachedElements.keys())[0])
                 self.cachedElements[user] = [session, time.time()]
         else: 
             self.cachedElements[user][1] = time.time()
 
     def get(self, user):
-        if self.exists(user):
+        if self.valid(user):
             return [user, self.cachedElements[user][0]]
         else:
             return None
 
-    def exists(self, user):
+    def valid(self, user):
         if user in self.cachedElements:
             if time.time() - self.cachedElements[user][1] < self.timeout:
                 return True
