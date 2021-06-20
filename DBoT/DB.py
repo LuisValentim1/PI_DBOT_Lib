17a124c3ea0b40c293e4483da8d3ca367344b3bd
unknown <lvalentim@ua.pt>
2021-06-20 23:17:04 +0100

    revert

--- DBoT/DB.py
+++ DBoT/DB.py
@@ -8,9 +8,9 @@ CONTACT_POINTS = ['10.0.12.65', '10.0.12.66']
 #---------------------------- ROLES AND PERSONAL KEYSPACE HANDLING ---------------------------------
 
 # Função que regista um utilizador, criar o seu keyspace pessoal e a role de forma a podermos utilizar a autenticação de cassandra 
-def register(user, passW, superUser, superPassword):
+def register(user, passW):
     auth_provider = PlainTextAuthProvider(
-        username=superUser, password=superPassword)
+        username='cassandra', password='cassandra')
     cluster = Cluster(contact_points=CONTACT_POINTS, auth_provider=auth_provider)
 
     session = cluster.connect('db')
